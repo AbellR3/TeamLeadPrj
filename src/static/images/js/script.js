f39928548c64e19548fcba48d25640a62f2c21c2
AbellR3 <66202988+AbellR3@users.noreply.github.com>
2021-03-05 18:04:54 +0400

    added swiper

---
+++ src/static/images/js/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container', {
+
+    direction: 'horizontal',
+    loop: true,
+
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+
+});
